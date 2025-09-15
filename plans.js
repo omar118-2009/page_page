baa130c3f96c03015c40822093958345db76b9b8
omar118-2009 <menisyomar5@gmail.com>
2025-09-15 21:28:47 +0300

    Update plans.js

--- plans.js
+++ plans.js
@@ -7,31 +7,39 @@
 
 
     // load
-   let PayBtn= document.getElementById('payment-btn')
+   let PayBtn=document.getElementById('payment-btn')
+   let PayBtn1=document.getElementById('payment-btn1')
   let loader=  document.getElementById('loading-wave')
   let main=  document.getElementById('main')
 
-PayBtn.onclick=function(){
-  main.style.display='none'
-  loader.style.display='flex'
-  loader.style.width='300px'
-  loader.style.height='100px'
-  loader.style.top='300px'
-  loader.style.left='550px'
-  loader.style.justifyContent='center'
- loader.style.alignItems = 'center';
+function load(btn){
+btn.onclick = function () {
+  main.style.display = 'none';
+  loader.style.display = 'flex';
+  loader.style.position = 'fixed'; // مهم جدًا للتوسيط
+  loader.style.width = '100%';
+  loader.style.height = '100%';
+  loader.style.marginTop = '25%';
+  loader.style.top = '50%';
+  loader.style.left = '50%';
+  loader.style.transform = 'translate(-50%, -50%)'; // يوسّط العنصر تمامًا
+  loader.style.justifyContent = 'center';
+  loader.style.alignItems = 'center';
 
- setTimeout(() => {
-  window.location.href = "payment.html";
 
- },1500)
 
-  
+  setTimeout(() => {
+    window.location.href = "payment.html";
+  }, 1500);
+};
 
 }
+  
+load(PayBtn)
+load(PayBtn1)
+
 
   
-
 
 
 
